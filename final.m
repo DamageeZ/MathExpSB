203265982fbee6ff8dc344367e0493e0c84113ed
DamageeZ <zhouzhm1@outlook.com>
2021-05-20 07:44:08 +0800

    0

--- final.m
+++ final.m
@@ -1,7 +1,41 @@
+clf;
+figure(1);
 syms f(x);
 f = sin(1.024*x)^2;
-fplot(f,'r-*')
+%fplot(f,'r-*')
 hold on;
 grid on;
-fplot(@(x)sin(x)^2,'b:^')
-axis([10,20,0,1])
+fplot(@(x)sin(1.024*x).^2,'r:','MarkerSize',1.5,'LineWidth',1.5)
+axis([0,pi/2,0,1])
+hold off;
+
+N = 100
+p1 = linspace(0,pi/2,N+1);
+for n = 1:N
+    spit1(n) = (p1(n+1)+ p1(n) )/2;
+end
+spit = sin(1.024.*spit1).^2;
+clf
+plot(spit1,spit,'r:','MarkerSize',1.5,'LineWidth',1.5)
+clear spit,p1,spit1;
+
+N = 200
+p1 = linspace(0,pi/2,N+1);
+for n = 1:N
+    spit1(n) = (p1(n+1)+ p1(n) )/2;
+end
+spit = sin(1.024.*spit1).^2;
+clf
+plot(spit1,spit,'r:','MarkerSize',1.5,'LineWidth',1.5)
+clear spit,p1,spit1;
+
+N = 1000
+p1 = linspace(0,pi/2,N+1);
+for n = 1:N
+    spit1(n) = (p1(n+1)+ p1(n) )/2;
+end
+spit = sin(1.024.*spit1).^2;
+clf
+plot(spit1,spit,'r:','MarkerSize',1.5,'LineWidth',1.5)
+clear spit,p1,spit1;
+
